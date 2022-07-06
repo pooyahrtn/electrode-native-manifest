ce99ed04c416b69940866a1e033d99aacd794060
Pooya Haratian <pooya.haratian@teampicnic.com>
2022-07-06 16:58:51 +0200

    Fix ReactNativeFlashListPackage plugin import

--- plugins/ern_v0.48.0+/@shopify/flash-list_v1.0.4+/FlashListPackagePlugin.java
+++ plugins/ern_v0.48.0+/@shopify/flash-list_v1.0.4+/FlashListPackagePlugin.java
@@ -6,11 +6,11 @@ import android.support.annotation.Nullable;
 
 import com.facebook.react.ReactInstanceManagerBuilder;
 import com.facebook.react.ReactPackage;
-import com.shopify.reactnative.flash_list.FlashListPackage;
+import com.shopify.reactnative.flash_list.ReactNativeFlashListPackage;
 
 public class FlashListPackagePlugin implements ReactPlugin {
     public ReactPackage hook(@NonNull Application application,
            @Nullable ReactPluginConfig config) {
-        return new FlashListPackage();
+        return new ReactNativeFlashListPackage();
     }
 }
